a759e2b37ed3fe8a3941be327c2ffb8d1577b10a
Mysta3 <myshawnestallings@gmail.com>
2020-04-04 15:58:06 -0700

    added sass, and initial homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
