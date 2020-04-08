dd082f5c1aa77a40ec210fec79f4eaf6f26a53ed
Mysta3 <myshawnestallings@gmail.com>
2020-04-07 19:11:56 -0700

    added react router and wrapped menuitem component in a withRouter HOF

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
-import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import HatsPage from './pages/hatspage/HatsPage';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
