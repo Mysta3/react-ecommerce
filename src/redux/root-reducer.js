d904ee9eac7ef6980adc2bd0d49f3afca3f99a5c
Mysta3 <myshawnestallings@gmail.com>
2020-08-28 14:13:30 -0700

    added redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,14 +1,23 @@
 // BASE REDUCER OBJECT THAT REPS ALL STATE OF APPLICATION
 // GLUE TO COMBINE ALL STATE TOGETHER
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
-
-import {combineReducers} from 'redux';
+//use localStorage as default storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], //the reducers we want to persist
+};
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
-})
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+export default persistReducer(persistConfig, rootReducer);
