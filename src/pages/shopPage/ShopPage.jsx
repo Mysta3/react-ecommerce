a0fcbc9305988cb22f8f96c4c2d863ef28b1886f
Mysta3 <myshawnestallings@gmail.com>
2020-09-09 17:45:05 -0700

    added lodash.memoize to shop_selectors, created nested routing in shop page, renamed category > collection, created collection id map to dynamically find params

--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import { Route} from 'react-router-dom';
 
+import CollectionPage from '../collection/Collection';
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 
 
 
 
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({match}) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route  exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route  path={`${match.path}/:categoryId`} component={CollectionPage}/>
   </div>
 );
 
