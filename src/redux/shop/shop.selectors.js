bc6b64adb2fecebca264a6dd0a4142140c52dbcf
Mysta3 <myshawnestallings@gmail.com>
2020-09-28 11:45:30 -0700

    moved directory/shop data into own reducer, created respective selectors, updated corresponding components with new redux flow for direcotry/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,17 +2,6 @@ import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
 
-//create collection map to identify string to id values
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
-
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -21,11 +10,16 @@ export const selectCollections = createSelector(
 );
 
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  //returns collections into an array form
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+
 //returns a curried function
 //a function that return another function
 export const selectCollection = memoize((collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    collections[collectionUrlParam]
   ));
